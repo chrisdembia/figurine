9464bb99d9d60fde4647c4c0efa21378a6ddd6c6
Adam Leeper <aleeper@willowgarage.com>
2013-02-28 11:07:00 -0800

    tweaks to build file

--- MicrophoneCables/buildAsy.sh
+++ MicrophoneCables/buildAsy.sh
@@ -14,3 +14,10 @@ fi
 
 pdflatex ${basefilename}.tex
 pdflatex ${basefilename}.tex
+
+echo "Done! Cleaning up..."
+# Cleanup some of the millions of asy files?
+rm ${basefilename}-1* *.out *.pre
+
+# Cleanup latex stuff?
+rm *.log *.aux *.synctex.gz 
